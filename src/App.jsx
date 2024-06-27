0730399d71528188065edf0f7f7373cbe1c7c4b7
T-Prakash2002 <prakasht1405@gmail.com>
2024-06-27 11:56:01 +0530

    basic nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      
+      <div className="container-fluid">
+        <div className="row">
+          <nav>
+            <div className="logo">
+              PRAKASH
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
